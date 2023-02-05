846896268ee101b34f0bbb084a82b0f4016f76cf
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-05 23:46:39 +0600

    Update GameEnity.java

--- src/GameEnity.java
+++ src/GameEnity.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class GameEnity {
+public class GameEnity {
+    private int health;
+    private int damage;
+    private String Protection;
+    public GameEnity(int health, int damage, String protection) {
+        this.health = health;
+        this.damage = damage;
+        Protection = protection;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public String getProtection() {
+        return Protection;
+    }
+    public void setProtection(String protection) {
+        Protection = protection;
+    }
 }
