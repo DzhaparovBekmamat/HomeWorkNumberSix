63d23c0227a9f167f0540dbc41b24575a9c0f3a8
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-05 23:47:25 +0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Boss {
+public class Boss extends GameEnity {
+    private String Weapon;
+    public Boss(int health, int damage, String protection) {
+        super(health, damage, protection);
+    }
+    public Boss(int health, int damage, String protection, String weapon) {
+        super(health, damage, protection);
+        this.Weapon = weapon;
+    }
+    public String getWeapon() {
+        return Weapon;
+    }
+    public void setWeapon(String weapon) {
+        this.Weapon = weapon;
+    }
 }
