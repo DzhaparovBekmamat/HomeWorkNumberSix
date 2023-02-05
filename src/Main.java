8b429d9ab7de1f08f856d4281dc75fa03b3c80de
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-05 23:48:13 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Boss boss1 = new Boss(700, 70, "Shield", "AK-47");
+        boss1.setHealth(700);
+        boss1.setDamage(70);
+        boss1.setProtection("Armor");
+        boss1.setWeapon("AK-47");
+        System.out.println(" | " + boss1.getHealth() + " > " + boss1.getDamage() + " > " + boss1.getProtection() + " > " + boss1.getWeapon() + "  |");
+        }
     }
-}
